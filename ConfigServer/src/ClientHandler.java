3b838dbefef249e1c5929d1c70ba9dfb56854fde
maciekkobierecki <maciekkobierecki@gmail.com>
2017-06-15 23:32:59 +0200

    0.0.2
    poprawki w obsłudze żądań

--- ConfigServer/src/ClientHandler.java
+++ ConfigServer/src/ClientHandler.java
@@ -1,12 +1,14 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ClientHandler implements Runnable {
@@ -27,21 +29,31 @@ public class ClientHandler implements Runnable {
 				Scanner in=new Scanner(inStream);
 				PrintWriter out=new PrintWriter(outStream,true);
 				
+				String lastWordOfLine;
+				String subStr;
 				while(in.hasNextLine()){
 					String line=in.nextLine();
-					switch(line){
-					case "config.properties":
+					lastWordOfLine=line.substring(line.lastIndexOf(" ")+1);
+					if(line.equals("GET CONFIG PROPERTIES"))
 						sendTextFromFile(out, "config.properties.txt");						
-						break;
-					case "maps":
+					else if(line.equals("GET MAPLIST"))
 						sendTextFromFile(out, "maps.txt");						
-						break;
-					case "highscores":
+					else if(line.equals("GET HIGHSCORES"))
 						sendTextFromFile(out, "highscores.txt");						
-						break;
-					default:
-						sendTextFromFile(out, line);	
+					else if(line.equals("GET LEVEL INFO "+lastWordOfLine)){
+						if(Integer.parseInt(lastWordOfLine) <= MainWindow.getAmountOfMaps())
+						sendTextFromFile(out, "map"+Integer.parseInt(lastWordOfLine)+".txt");	
 					}
+					else if(line.equals("GAME SCORE")){
+						String name=in.nextLine();
+						int score=Integer.parseInt(in.nextLine());
+						if(addScore(name,score))
+							out.println("GAME SCORE ACCEPTED");
+						else
+							out.println("GAME SCORE NOT ACCEPTED");
+							
+					}
+					
 				}
 				out.close();
 			}
@@ -59,7 +71,54 @@ public class ClientHandler implements Runnable {
 		
 		
 	}
+	public static Boolean addScore(String name, int score){
 	
+			ArrayList<String>rank=loadHighscoresToArrayList();
+			for(int i=0; i<rank.size(); i+=2){
+				if(i+1<rank.size()){
+					int scoreFromHighscores=Integer.parseInt(rank.get(i+1));
+					if(scoreFromHighscores<score){
+						rank.add(i, Integer.toString(score));
+						rank.add(i, name);
+						rank.remove(rank.size()-1);
+						rank.remove(rank.size()-1);
+						saveHighscoresToFile(rank);
+						return true;					
+					}
+			}		
+		}
+		return false;
+	}
+	public static ArrayList<String> loadHighscoresToArrayList(){
+		ArrayList<String> rank=new ArrayList<>();
+		try(BufferedReader br=new BufferedReader(new FileReader("highscores.txt"))) {
+			String line;
+			while((line=br.readLine())!=null){
+				rank.add(line);
+			}
+		}
+		catch (Exception e){
+			System.out.println(e.getMessage());
+		}
+		return rank;
+	}
+	public static void saveHighscoresToFile(ArrayList<String> highscores){
+		File fold = new File("highscores.txt");
+		fold.delete();
+		File fnew= new File("highscores.txt");
+		try{
+			FileWriter f2=new FileWriter(fnew, false);
+			BufferedWriter bw=new BufferedWriter(f2);
+			for(int i=0; i<highscores.size(); i++){
+				bw.write(highscores.get(i));
+				bw.newLine();
+			}
+			bw.close();
+		}		
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
 	public void sendTextFromFile(PrintWriter out, String fileName){
 		try(BufferedReader br=new BufferedReader(new FileReader(fileName))) {
 			String line;
--- ConfigServer/src/MainWindow.java
+++ ConfigServer/src/MainWindow.java
@@ -2,45 +2,54 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JSpinner;
+import javax.swing.JSpinner.DefaultEditor;
 import javax.swing.JTextField;
 
 public class MainWindow extends JFrame implements ActionListener, ServerStatusListener{
 
 	private JButton startButton;
-	private JLabel infoLabel, portLabel;
+	private JLabel infoLabel, portLabel, amountMapsLabel;
 	private JTextField portField;
+	private JSpinner amountMaps;
 	private Boolean isRunning;
+	private static int amountOfMaps;
 	ConfigServer server;
 	MainWindow(){
 		super("Dyna Blaster Server");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setPreferredSize(new Dimension(300,150));
 		infoLabel=new JLabel("not running");
+		amountMapsLabel=new JLabel("amount of maps:");
+		amountMaps=new JSpinner();
+		((DefaultEditor) amountMaps.getEditor()).getTextField().setEditable(false);
 		server=null;
 		isRunning=false;
 		startButton=new JButton("start");
 		startButton.addActionListener(this);
 		portField=new JTextField();
 		portField.setPreferredSize(new Dimension(100,30));
-		this.setLayout(new GridLayout(2,2));
+		this.setLayout(new GridLayout(3,2));
 		portLabel=new JLabel();
 		portLabel.setText("port:");
 		portLabel.setHorizontalAlignment(JLabel.CENTER);
 		infoLabel.setHorizontalAlignment(JLabel.CENTER);
 		add(portLabel);
 		add(portField);
+		add(amountMapsLabel);
+		add(amountMaps);
 		add(infoLabel);
-		add(startButton,0,3);
-
+		add(startButton,0,5);
 		pack();
 		setVisible(true);
 	}
-	
+	public static int getAmountOfMaps(){
+		return amountOfMaps;
+	}
 	public void turnON(){
 		if(!isRunning){
 			server= new ConfigServer(Integer.parseInt(portField.getText()));
@@ -53,6 +62,7 @@ public class MainWindow extends JFrame implements ActionListener, ServerStatusLi
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		amountOfMaps=(Integer)amountMaps.getValue();
 		turnON();
 		
 	}
